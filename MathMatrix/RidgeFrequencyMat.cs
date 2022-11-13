d7600d10d1c4f858af6d0536746d863263e6fa79
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-13 18:20:52 +0700

    prototype class

---
+++ MathMatrix/RidgeFrequencyMat.cs
@@ -0,0 +1,15 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FingerprintRecognition.MathMatrix
+{
+    internal class RidgeFrequencyMat
+    {
+        static public double[,] Create(ref Image<Gray, double> norm, ref bool[,] msk, ref double[,] orient, int blockSize, int kernelSize, int minWaveLength, int maxWavelength)
+        {
+            double[,] res = new double[msk.GetLength(0), msk.GetLength(1)];
+
+            return res;
+        }
+    }
+}
