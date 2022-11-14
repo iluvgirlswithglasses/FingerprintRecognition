aa9efbc8232ae1b16d546766b23caa1079037c1e
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-14 10:53:59 +0700

    the most useful of all, being discovered too late

---
+++ Tool/Iterator2D.cs
@@ -0,0 +1,19 @@
+﻿
+namespace FingerprintRecognition.Tool
+{
+    internal class Iterator2D
+    {
+        /** @ the core */
+        static public void Forward(int h, int w, Func<int, int, bool> f)
+        {
+            Forward(0, 0, h, w, f);
+        }
+
+        static public void Forward(int t, int l, int d, int r, Func<int, int, bool> f)
+        {
+            for (int y = t; y < d; y++)
+                for (int x = l; x < r; x++)
+                    f(y, x);
+        }
+    }
+}
