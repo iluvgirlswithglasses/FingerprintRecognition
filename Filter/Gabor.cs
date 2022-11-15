e12aa47f22c6b1d34d5a3dea93815b4686beeccf
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-15 18:21:14 +0700

    wtf

---
+++ Filter/Gabor.cs
@@ -0,0 +1,55 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using FingerprintRecognition.Tool;
+using static System.Math;
+
+namespace FingerprintRecognition.Filter {
+    internal class Gabor {
+        static public bool[,] Create(Image<Gray, double> norm, double[,] orient, double[,] freq) {
+            const int ANGLE_INC = 3;
+
+            int h = norm.Height, w = norm.Width;
+            bool[,] res = new bool[h, w];
+
+            HashSet<double> uniqueFreq = new HashSet<double>();
+            MatTool<double>.Forward(ref freq, (y, x, val) => {
+                if (val > 0) {
+                    // round to nearest 0.01
+                    // to reduce unique numbers
+                    double rnd = Round(val * 100) / 100.0;
+                    uniqueFreq.Add(rnd);
+                }
+                return true;
+            });
+
+            // Generate filters corresponding to these distinct frequencies and
+            // orientations in 'ANGLE_INC' increments.
+            double[] sigma = new double[uniqueFreq.Count];
+            double sigmaMax = 0.0;
+            int _counter = 0;
+            foreach (double i in uniqueFreq) {
+                sigma[_counter] = 1 / i * 0.65;
+                sigmaMax = Max(sigmaMax, sigma[_counter]);
+                _counter++;
+            }
+            int blockSize = (int)Round(3.0 * sigmaMax);
+            double[,] sigmaMaskX = new double[blockSize<<1|1, blockSize<<1|1];
+            double[,] sigmaMaskY = new double[blockSize<<1|1, blockSize<<1|1];
+            MatTool<double>.Forward(ref sigmaMaskX, (y, x, val) => {
+                sigmaMaskX[y, x] = (x - blockSize) * (x - blockSize);
+                sigmaMaskY[y, x] = (y - blockSize) * (y - blockSize);
+                return true;
+            });
+
+            
+
+            // gabor filter equation
+            // reffilter = refa * refb
+            // refa = - ( sigmaMaskX/(sigma**2) + sigmaMaskY/(sigma**2) )
+            // refb = cos( 2 * PI * uniqueFreq[0] * sigmaMaskX )
+            
+
+            return res;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -40,15 +40,11 @@ double[,] orient = OrientMat.Create(norm, BLOCK_SIZE);
 norm = Normalization.ExcludeBackground(norm, segmentMask, BLOCK_SIZE);
 // get frequency
 double[,] freq = RidgeFrequencyMat.Create(norm, segmentMask, orient, BLOCK_SIZE, 5);
+// gabor filter
+bool[,] gabor = Gabor.Create(norm, orient, freq);
 
 /** @ debug */
 CvInvoke.Imwrite(CWD + "sample-images-o\\normalized.jpg", norm);
 CvInvoke.Imwrite(CWD + "sample-images-o\\segmented.jpg", ToImage.FromDoubleImage(segmented));
 Image<Gray, byte> orientImg = OrientMat.Visualize(segmented, segmentMask, orient, BLOCK_SIZE);
 CvInvoke.Imwrite(CWD + "sample-images-o\\orient-img.jpg", orientImg);
-
-MatTool<double>.Forward(ref freq, (y, x, val) => {
-    if (val > 0)
-        Console.WriteLine(String.Format("{0}-{1} f = {2}", y * BLOCK_SIZE, x * BLOCK_SIZE, val));
-    return true;
-});
