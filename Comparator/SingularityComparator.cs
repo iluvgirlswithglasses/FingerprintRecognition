cfd68a75e834789d2f70ad346b565a37c4abca57
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-30 15:57:54 +0700

    significant singularities  comparision is almost finished

--- Comparator/SingularityComparator.cs
+++ Comparator/SingularityComparator.cs
@@ -8,16 +8,31 @@ namespace FingerprintRecognition.Comparator {
         // if all singularities matches
         public bool SMatches = false;
 
-        public SingularityComparator(SingularityManager mgrA, SingularityManager mgrB, bool[,] skeA, bool[,] skeB) {
+        public int ASCnt;   // how many singularities are there in fingerprint A
+        public double ASLen;    // the distance between the first two singularities of A
+        public double BSLen;    // the distance between the first two singularities of B
+
+        public SingularityComparator(
+            SingularityManager mgrA, SingularityManager mgrB, bool[,] skeA, bool[,] skeB,
+            double distanceTolerance, double angleTolerence
+        ) {
             if (
                 mgrA.Loops.Count != mgrB.Loops.Count || 
                 mgrA.Whorls.Count != mgrB.Whorls.Count || 
                 mgrA.Deltas.Count != mgrB.Deltas.Count
             ) {
                 SMatches = false;
-            } else {
-                
+                return;
             }
+
+            ASCnt = mgrA.Loops.Count + mgrA.Deltas.Count + mgrA.Whorls.Count;
+            if (ASCnt == 1) {
+                SMatches = true;
+            } else {
+
+            }
+
+            // perform further comparison here
         }
 
         
