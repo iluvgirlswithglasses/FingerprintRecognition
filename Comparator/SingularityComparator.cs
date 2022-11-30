637b3706d3edaf1112280e65b19c4ba012a8e97d
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-30 16:55:42 +0700

    singularities comparison

--- Comparator/SingularityComparator.cs
+++ Comparator/SingularityComparator.cs
@@ -7,14 +7,21 @@ namespace FingerprintRecognition.Comparator {
 
         // if all singularities matches
         public bool SMatches = false;
+        //
+        public double SLenMismatchScore = 0.0;
+        public double SAngleMismatchScore = 0.0;
 
         public int ASCnt;   // how many singularities are there in fingerprint A
         public double ASLen;    // the distance between the first two singularities of A
         public double BSLen;    // the distance between the first two singularities of B
 
+        // let alpha be the angle of the vector that connect the st & the nd singu of A
+        // let  beta ------------------------------- connect the st & the nd singu of B
+        // AngleDiff is beta - alpha
+        public double AngleDiff;
+
         public SingularityComparator(
-            SingularityManager mgrA, SingularityManager mgrB, bool[,] skeA, bool[,] skeB,
-            double distanceTolerance, double angleTolerence
+            SingularityManager mgrA, SingularityManager mgrB, bool[,] skeA, bool[,] skeB
         ) {
             if (
                 mgrA.Loops.Count != mgrB.Loops.Count || 
@@ -25,16 +32,59 @@ namespace FingerprintRecognition.Comparator {
                 return;
             }
 
-            ASCnt = mgrA.Loops.Count + mgrA.Deltas.Count + mgrA.Whorls.Count;
+            ASCnt = mgrA.SingularLst.Count;
             if (ASCnt == 1) {
                 SMatches = true;
             } else {
-
+                SMatches = CompareSingularity(mgrA.SingularLst, mgrB.SingularLst);
             }
 
-            // perform further comparison here
+            // perform endings, bifurs, and ridges count comparison here
         }
 
-        
+        // assert that ASCnt == BSCnt && ASCnt >= 2
+        // a[i]: { the position of the `i-th` singularity, the type of that singularity }
+        private bool CompareSingularity(List<Pair<Pair<int, int>, int>> a, List<Pair<Pair<int, int>, int>> b) {
+            for (int i = 0; i < ASCnt; i++)
+                // these core points does not allign
+                if (a[i].Nd != b[i].Nd)
+                    return false;
+            //
+            Pair<double, double> u = GetVector(a[0].St, a[1].St),
+                                 v = GetVector(b[0].St, b[1].St);
+            ASLen = CalcLen(u);
+            BSLen = CalcLen(v);
+            AngleDiff = CalcAlpha(v) - CalcAlpha(u);
+            //
+            for (int i = 1; i < ASCnt - 1; i++) {
+                u = GetVector(a[i].St, a[i + 1].St);
+                v = GetVector(b[i].St, b[i + 1].St);
+                SLenMismatchScore += Abs(CalcLen(u) / ASLen - CalcLen(v) / BSLen);
+                //   (a - a0) - (b - b0)
+                // = a - b - a0 + b0
+                // = a - b + AngleDiff
+                SAngleMismatchScore += Abs(CalcAlpha(u) - CalcAlpha(v) + AngleDiff);
+            }
+            return true;
+        }
+
+        /** 
+         * @ shortcuts
+         * */
+        private Pair<double, double> GetVector(Pair<int, int> a, Pair<int, int> b) {
+            return new(b.St - a.St, b.Nd - a.Nd);
+        }
+
+        private Pair<double, double> GetVector(Pair<double, double> a, Pair<double, double> b) {
+            return new(b.St - a.St, b.Nd - a.Nd);
+        }
+
+        private double CalcLen(Pair<double, double> v) {
+            return Sqrt(v.St * v.St + v.Nd * v.Nd);
+        }
+
+        private double CalcAlpha(Pair<double, double> v) {
+            return Atan2(v.St, v.Nd);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@
 using Emgu.CV;
 using Emgu.CV.Structure;
 using FingerprintRecognition.Comparator;
-using FingerprintRecognition.DataStructure;
 
 /** @ program parameters */
 const int BLOCK_SIZE = 16;
@@ -18,15 +17,25 @@ const double USEFUL_RADIUS = 0.9;
 /** @ temporary constants */
 const string IN  = "D:\\r\\siglaz\\FingerprintRecognition\\sample-images\\";
 const string OUT = "D:\\r\\siglaz\\FingerprintRecognition\\sample-images-o\\";
+const int IM_COUNT = 11;
 
 /** @ get files */
-for (int i = 0; i < 10; i++) {
+FImage[] imgs = new FImage[IM_COUNT];
+for (int i = 0; i < IM_COUNT; i++) {
     Console.WriteLine("Processing file indexed " + i.ToString());
-    FImage target = new(new Image<Gray, byte>(IN + string.Format("{0}.jpg", i)), BLOCK_SIZE, USEFUL_RADIUS);
-    foreach (var j in target.SingularMgr.SingularLst)
-        Console.WriteLine(j);
-    // target.DisplaySingularity(i.ToString());
-    // CvInvoke.Imwrite(string.Format(OUT + "mask-{0}.png", i), ToImage.FromBinaryArray(target.SegmentMask));
+    imgs[i] = new(new Image<Gray, byte>(IN + string.Format("{0}.jpg", i)), BLOCK_SIZE, USEFUL_RADIUS);
+    imgs[i].DisplaySingularity(i.ToString());
     Console.WriteLine();
 }
 
+for (int i = 0; i < IM_COUNT; i++) {
+    for (int j = i + 1; j < IM_COUNT; j++) {
+        SingularityComparator cmp = new SingularityComparator(
+            imgs[i].SingularMgr, imgs[j].SingularMgr, imgs[i].Skeleton, imgs[j].Skeleton
+        );
+        Console.WriteLine(
+            "Img {0} & {1}: Singularities match = {2}, angle diff = {3}, dist diff = {4}", 
+            i, j, cmp.SMatches, cmp.SAngleMismatchScore, cmp.SLenMismatchScore
+        );
+    }
+}
